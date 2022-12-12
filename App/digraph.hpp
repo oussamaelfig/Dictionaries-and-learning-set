27985297334dbd0a674151d78a528a33b0d2bdd9
Sékou Junior Diakite <50834538+Jusek98@users.noreply.github.com>
2022-12-12 04:15:23 -0500

    fonction reduction basse faite

--- App/digraph.hpp
+++ App/digraph.hpp
@@ -330,8 +330,42 @@ bool Digraph<T>::acyclique(T u, T v) const
 template <class T>
 bool Digraph<T>::reduction_base()
 {
-	if (boucle)
-		return false;
+	  int cpt=0;
+    std::list<T> listesommet;
+    
+
+
+    for (const auto &p: graphe){
+        if (puits(p.first) || boucle(p.first)) {
+             listesommet.push_back(p.first);
+            cpt++;
+            for (const auto &sommet: predecesseurs(p.first)) {
+
+                supprimer_arc(sommet,p.first);
+                if (puits(sommet)){
+                 cpt++;
+                 listesommet.push_back(sommet);
+                }
+
+
+            }
+
+
+        }
+
+
+
+        }
+
+
+    for (T sommet:listesommet) {
+        supprimer_sommet(sommet);
+    }
+
+
+      int nbrSommetRestant =nb_sommets(); // nombre de sommet restant
+      int nbrSommetSupprimer=cpt; // nombre de sommet supprimer
+    return false;
 }
 
 template <class T>
